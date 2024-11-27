6a8d2b0e14ad18e76131af343b3b4092a14492bd
Binary <binaris000@gmail.com>
2024-11-27 09:47:41 -0400

    Change to light/dark mode with the status bar!

--- src/main.ts
+++ src/main.ts
@@ -1,79 +1,39 @@
 import { Editor, MarkdownView, Notice, Plugin } from 'obsidian';
-import { MyPluginSettings, SampleSettingTab } from './settings/settings_main';
+import { DEFAULT_SETTINGS, IMultiEventSettings, MultiEventSettingTab } from './settings/settings_main';
 import { SampleModal } from './components/base_modals';
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export default class MultiBasicEvents extends Plugin {
+	settings: IMultiEventSettings;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	DARK_MODE_THEME_KEY: string = "obsidian";
+	LIGHT_MODE_THEME_KEY: string = "moonstone";
+
+	currentColorStatus: string;
 
 	async onload() {
 		await this.loadSettings();
 
-		new Notice('Loading Bin-BasicMultiEvents!');
+		this.initColorStatus();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new SampleModal(this.app).open();
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		const themePickerStatusBarItem: HTMLElement = this.addStatusBarItem();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+		const changeThemeButton: HTMLElement = themePickerStatusBarItem.createDiv({
+			cls: "status-bar-item mod-clickable",
+			text: this.currentColorStatus
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		changeThemeButton.addEventListener("click", () => {
+			this.toggleColorScheme();
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerEvent(
+			this.app.workspace.on("css-change", () => {
+				this.initColorStatus();
+				changeThemeButton.textContent = this.currentColorStatus;
+			})
+		);
+
+		this.addSettingTab(new MultiEventSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -86,5 +46,35 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+
+
+
+	isDarkMode(): boolean {
+		//@ts-ignore
+		return this.app.vault.getConfig("theme") === this.DARK_MODE_THEME_KEY;
+	}
+
+	toggleColorScheme(): void {
+		let colorSchemeKey;
+
+		if (this.isDarkMode()) {
+			colorSchemeKey = this.LIGHT_MODE_THEME_KEY;
+			this.currentColorStatus = this.settings.lightModeStringStatus;
+		} else {
+			colorSchemeKey = this.DARK_MODE_THEME_KEY;
+			this.currentColorStatus = this.settings.darkModeStringStatus;
+		}
+
+		//@ts-ignore
+		this.app.changeTheme(colorSchemeKey);
+	}
+
+	initColorStatus(): void {
+		if(this.isDarkMode()){
+			this.currentColorStatus = this.settings.lightModeStringStatus;
+		} else {
+			this.currentColorStatus = this.settings.darkModeStringStatus;
+		}
 	}
 }
--- src/settings/settings_main.ts
+++ src/settings/settings_main.ts
@@ -1,19 +1,37 @@
 import { PluginSettingTab, App, Setting } from "obsidian";
-import MyPlugin from "src/main";
+import MultiBasicEvents from "src/main";
 
 
-export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export interface IMultiEventSettings {
+	mySetting: string;
 
-	constructor(app: App, plugin: MyPlugin) {
+	darkModeStringStatus: string;
+	lightModeStringStatus: string;
+}
+
+
+export class MultiEventSettingTab extends PluginSettingTab {
+	plugin: MultiBasicEvents;
+
+	constructor(app: App, plugin: MultiBasicEvents) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
 		const { containerEl } = this;
-
+		
 		containerEl.empty();
+
+		containerEl.createEl("h1", { text: "Multi-Events" });
+		containerEl.createEl("h2", { text: "Multi-Events" });
+		containerEl.createEl("h3", { text: "Multi-Events" });
+		containerEl.createEl("h4", { text: "Multi-Events" });
+		containerEl.createEl("h5", { text: "Multi-Events" });
+		containerEl.createEl("h6", { text: "Multi-Events" });
+		
+		this.containerEl.createEl("br");
+
 
 		new Setting(containerEl)
 			.setName('Setting #1')
@@ -25,10 +43,36 @@ export class SampleSettingTab extends PluginSettingTab {
 					this.plugin.settings.mySetting = value;
 					await this.plugin.saveSettings();
 				}));
+
+		
+		new Setting(containerEl)
+			.setName('Dark Mode String Status')
+			.setDesc('Set any text-emoji to display the dark status!')
+			.addText(text => text
+				.setPlaceholder('🌕')
+				.setValue(this.plugin.settings.darkModeStringStatus)
+				.onChange(async (value) => {
+					this.plugin.settings.darkModeStringStatus = value;
+					await this.plugin.saveSettings();
+				}));
+		
+
+		new Setting(containerEl)
+			.setName('Light Mode String Status')
+			.setDesc('Set any text-emoji to display the light status!')
+			.addText(text => text
+				.setPlaceholder('🔆')
+				.setValue(this.plugin.settings.lightModeStringStatus)
+				.onChange(async (value) => {
+					this.plugin.settings.lightModeStringStatus = value;
+					await this.plugin.saveSettings();
+				}));
 	}
 }
-// Remember to rename these classes and interfaces!
-export interface MyPluginSettings {
-	mySetting: string;
-}
+
+export const DEFAULT_SETTINGS: IMultiEventSettings = {
+	mySetting: 'default',
+	darkModeStringStatus: '🌕',
+	lightModeStringStatus: '🔆',
+};
 
